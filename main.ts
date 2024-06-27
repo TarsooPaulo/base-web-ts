dbbb57acbcc0dfc61cffc240a510f1e41c4cce7c
TarsooPaulo <paulodetarse333@gmail.com>
2024-06-27 11:21:58 -0300

    update project

---
+++ main.ts
@@ -0,0 +1,9 @@
+// entrypoint
+console.log('hello world');
+console.log('abc');
+console.log(123);
+console.log('oi');
+
+class pessoa {
+  nome: string;
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+const path = require('path');
+
+module.exports = {
+  entry: './main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'src', 'js'),
+  },
+};
